8ac6d81ad67c5b098a01d2c92999d42dba543f9f
Sibghatullah Durrani <sibghatullahdurrani007@gmail.com>
2026-04-01 04:04:58 +0500

    backup

--- lua/dev/plugins/lsp/lsp.lua
+++ lua/dev/plugins/lsp/lsp.lua
@@ -1,38 +1,66 @@
 return {
-	"hrsh7th/cmp-nvim-lsp",
-	event = { "BufReadPre", "BufNewFile" },
-	dependencies = {
-		{ "antosha417/nvim-lsp-file-operations", config = true },
-		{ "folke/lazydev.nvim", opts = {} },
-	},
-	config = function()
-		-- import cmp-nvim-lsp plugin
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+  "hrsh7th/cmp-nvim-lsp",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    { "folke/lazydev.nvim",                  opts = {} },
+  },
+  config = function()
+    -- import cmp-nvim-lsp plugin
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-		-- used to enable autocompletion (assign to every lsp server config)
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+    -- used to enable autocompletion (assign to every lsp server config)
+    local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		vim.lsp.config("*", {
-			capabilities = capabilities,
-		})
+    vim.lsp.config("*", {
+      capabilities = capabilities,
+    })
 
-		-- Setup Dart language server (only if Flutter is not installed)
-		if vim.fn.executable("flutter") == 0 then
-			vim.lsp.config("dartls", {
-				cmd = { "dart", "language-server", "--protocol=lsp" },
-				filetypes = { "dart" },
-				root_markers = { "pubspec.yaml" },
-				capabilities = capabilities,
-				settings = {
-					dart = {
-						completeFunctionCalls = true,
-						showTodos = true,
-						enableSnippets = true,
-						updateImportsOnRename = true,
-					},
-				},
-			})
-			vim.lsp.enable("dartls")
-		end
-	end,
+    -- Setup clangd for C++23
+    vim.lsp.config("clangd", {
+      cmd = {
+        "clangd",
+        "--background-index",
+        "--clang-tidy",
+        "--header-insertion=iwyu",
+        "--completion-style=detailed",
+        "--function-arg-placeholders",
+        "--fallback-style=llvm",
+      },
+      filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+      root_markers = {
+        ".clangd",
+        ".clang-tidy",
+        ".clang-format",
+        "compile_commands.json",
+        "compile_flags.txt",
+        "configure.ac",
+        ".git",
+      },
+      capabilities = capabilities,
+      init_options = {
+        fallbackFlags = { "-std=c++23" },
+      },
+    })
+    vim.lsp.enable("clangd")
+
+    -- Setup Dart language server (only if Flutter is not installed)
+    if vim.fn.executable("flutter") == 0 then
+      vim.lsp.config("dartls", {
+        cmd = { "dart", "language-server", "--protocol=lsp" },
+        filetypes = { "dart" },
+        root_markers = { "pubspec.yaml" },
+        capabilities = capabilities,
+        settings = {
+          dart = {
+            completeFunctionCalls = true,
+            showTodos = true,
+            enableSnippets = true,
+            updateImportsOnRename = true,
+          },
+        },
+      })
+      vim.lsp.enable("dartls")
+    end
+  end,
 }
--- lua/dev/plugins/lsp/mason.lua
+++ lua/dev/plugins/lsp/mason.lua
@@ -1,52 +1,53 @@
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			-- list of servers for mason to install
-			ensure_installed = {
-				"ts_ls",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"lua_ls",
-				"pyright",
-				"eslint",
-				"gopls",
-				"golangci_lint_ls",
-				"protols",
-				"ast_grep",
-			},
-		},
-		dependencies = {
-			{
-				"williamboman/mason.nvim",
-				opts = {
-					ui = {
-						icons = {
-							package_installed = "✓",
-							package_pending = "➜",
-							package_uninstalled = "✗",
-						},
-					},
-				},
-			},
-			"neovim/nvim-lspconfig",
-		},
-	},
-	{
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-		opts = {
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"pylint",
-				"eslint_d",
-			},
-		},
-		dependencies = {
-			"williamboman/mason.nvim",
-		},
-	},
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      -- list of servers for mason to install
+      ensure_installed = {
+        "ts_ls",
+        "html",
+        "cssls",
+        "tailwindcss",
+        "lua_ls",
+        "pyright",
+        "eslint",
+        "gopls",
+        "golangci_lint_ls",
+        "protols",
+        "ast_grep",
+        "clangd",
+      },
+    },
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = {
+          ui = {
+            icons = {
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗",
+            },
+          },
+        },
+      },
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = {
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
+        "eslint_d",
+      },
+    },
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+  },
 }
